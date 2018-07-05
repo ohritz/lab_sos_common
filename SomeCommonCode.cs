4b67e834d5c795099ac66ac0a51d2230a9e869eb
Sohan Fernando <sohan.fernando@preciofishbone.se>
2018-07-05 09:49:41 +0200

    added som code

---
+++ SomeCommonCode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common
+{
+    public static class SomeCommonCode
+    {
+        public static int AddSome(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
